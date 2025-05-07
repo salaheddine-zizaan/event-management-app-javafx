c34713a7f2fa5cd638622cae68a44c1f588d75de
salaheddine-zizaan <169148620+salaheddine-zizaan@users.noreply.github.com>
2025-05-07 15:54:11 +0100

    home interface added

--- src/main/java/org/example/eventmanagement/Controller/HelloController.java
+++ src/main/java/org/example/eventmanagement/Controller/HelloController.java
@@ -11,4 +11,12 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    private void goToRegister(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/eventmanagement/view/auth/register-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
