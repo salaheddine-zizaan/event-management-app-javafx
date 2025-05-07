c02759be96297405cbad6aa1b1cc8117a2aea861
salaheddine-zizaan <169148620+salaheddine-zizaan@users.noreply.github.com>
2025-05-07 02:50:31 +0100

    home interface added

--- src/main/java/org/example/eventmanagement/Controller/HelloController.java
+++ src/main/java/org/example/eventmanagement/Controller/HelloController.java
@@ -3,34 +3,12 @@ package org.example.eventmanagement.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    private void goToLogin(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/eventmanagement/view/auth/login-view.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-    @FXML
-    private void goToRegister(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/eventmanagement/view/auth/register-view.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
