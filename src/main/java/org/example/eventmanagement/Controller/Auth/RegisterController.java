d42fa7cc1c5a63304a58ec6d19f3e75c88b1c3d6
todolist <chbabmohamed3@gmail.com>
2025-05-09 17:30:13 +0100

    scene management and view management work! now I m going to add the rest of views

--- src/main/java/org/example/eventmanagement/Controller/Auth/RegisterController.java
+++ src/main/java/org/example/eventmanagement/Controller/Auth/RegisterController.java
@@ -26,7 +26,7 @@ public class RegisterController {
 
     @FXML
     public void initialize() {
-        roleComboBox.getItems().addAll( "organizer", "user");
+        roleComboBox.getItems().addAll( "organizer", "user", "admin");
     }
 
     @FXML
